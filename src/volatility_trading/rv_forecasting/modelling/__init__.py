d5aa46eac6391db712fb1bf7b5e9693db83ab851
anthonymakarewicz <anthonymakarewicz@gmail.com>
2025-11-13 06:54:16 +0100

    Added evaluation module

--- src/volatility_trading/rv_forecasting/modelling/__init__.py
+++ src/volatility_trading/rv_forecasting/modelling/__init__.py
@@ -1,12 +1,14 @@
-from .feature_importance import (
-    in_sample_stability, single_feature_importance, oos_perm_importance
-)
 from .data_processing import DataProcessor
 from .cross_validation import PurgedKFold
 from .walk_forward import WalkForwardOOS
 from .metrics import compute_metrics
+from .evaluation import eval_ensembles, eval_model_cv
+from .feature_importance import (
+    in_sample_stability, single_feature_importance, oos_perm_importance
+)
 
 __all__ = [
     "DataProcessor", "PurgedKFold", "WalkForwardOOS", "compute_metrics",
-    "in_sample_stability", "single_feature_importance", "oos_perm_importance"
+    "in_sample_stability", "single_feature_importance", "oos_perm_importance",
+    "eval_model_cv", "eval_ensembles"
 ]
